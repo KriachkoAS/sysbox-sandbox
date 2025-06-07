8b39afad07b31fc1b57035d2e3e84689f6f6ce3d
Andrii Kriachko <kriachko.a.s@nmu.one>
2025-06-07 10:40:05 +0300

    scripted usage

---
+++ vscode/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z ${1+1} ]; then
+	path="."
+else
+	path=$1
+fi
+vscode_commit=$(code --no-sandbox --user-data-dir /tmp -v | sed -n '2p')
+compose_path="/some/path"
+WORKSPACE_PATH=$path \
+VSCODE_COMMIT=$vscode_commit \
+docker compose \
+	-f $compose_path
+	build
+
+WORKSPACE_PATH=$path \
+VSCODE_COMMIT=$vscode_commit \
+docker compose \
+	-f $compose_path \
+	up
