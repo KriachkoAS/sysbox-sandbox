053990daf32b4fe9367f368b783606f10d4030cb
Andrii Kriachko <kriachko.a.s@nmu.one>
2025-06-08 08:45:53 +0300

    imported git config

--- vscode/run.sh
+++ vscode/run.sh
@@ -6,13 +6,16 @@ else
 fi
 vscode_commit=$(code --no-sandbox --user-data-dir /tmp -v | sed -n '2p')
 compose_path="/some/path"
+home_dir="/home/username"
 
+HOME_DIR=$home_dir \
 WORKSPACE_PATH=$path \
 VSCODE_COMMIT=$vscode_commit \
 docker compose \
 	-f $compose_path
 	build
 
+HOME_DIR=$home_dir \
 WORKSPACE_PATH=$path \
 VSCODE_COMMIT=$vscode_commit \
 docker compose \
